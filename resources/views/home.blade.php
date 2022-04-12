716fc2c86e67b485643c7362ed8772666e4900e8
afanfian <afanfian14@gmail.com>
2022-04-12 22:43:54 +0700

    Update landing page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('main')
+
+@section('title', 'Home')
+
+<section class="home" id="Home">
+    <div class="container-lg py-4">
+        <div class="row logo">
+            <div class="col-md-6">
+                <img src="img/laravel.png" alt="Logo Saviestance" width="250px" style="margin-left: 10%;">
+            </div>
+        </div>
+        <div class="row saviestance" style="color: white">
+            <div class="col-md-8" >
+                <p style="font-size: 4em; font-weight: bold;">
+                    Fian Awamiry Maulana
+                </p>
+                <p style="font-size: 1.2em; font-weight:bold;">
+                    5025201035
+                </p>
+                <p style="font-size: 1.5em; font-weight: bold;">
+                    CURD - Pemrograman Berbasis Kerangka Kerja (B)
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,15 @@
+{{-- CSS --}}
+<link rel="stylesheet" href="/css/navbar.css">
+{{-- Font Poppins --}}
+<link href='https://fonts.googleapis.com/css?family=Poppins' rel='stylesheet'>
+<nav>
+    <ul>
+        <li><a href="/home" class="nav-li">Home</a></li>
+        <li><a href="/main" class="nav-li">Form CRUD</a></li>
+        <li><a href="/login" class="nav-li">PPDB SD</a></li>
+        <li><a href="/article" class="nav-li">PPDB SMP</a></li>
+    </ul>
+</nav>
+<div class="container mt-5">
+    @yield('container')
+</div>
