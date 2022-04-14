aad0fa25fbdba2e0ef080e906f4dd6642a901b39
afanfian <afanfian14@gmail.com>
2022-04-15 06:40:56 +0700

    Update PPDB SD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PpdbsdController;
+use App\Models\ppdbsd;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +18,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', function () {
-    return view('home');
+Route::group(['prefix' => 'ppdbsd', 'as' => 'ppdbsd.'], function () {
+    Route::get('/', [PpdbsdController::class, 'index'])->name('home');
 });
+// Route menampilkan home
+Route::get('/', [PpdbsdController::class, 'index'])->name('home');
+// Tmabah Data dan ngecreate data pada ppdbsd controller
+// Buat Data dan ngestore data pada ppdbsd controller
+Route::get('/buat', [PpdbsdController::class, 'create'])->name('ppdbsd.tambah-data');
+Route::post('/buat-data', [PpdbsdController::class, 'store'])->name('ppdbsd.buat-data');
+// Edit data dan ngedit data pada ppdbsd controller
+Route::get('/edit/{id}', [PpdbsdController::class, 'edit'])->name('ppdbsd.edit');
+Route::post('/update/{id}', [PpdbsdController::class, 'update'])->name('ppdbsd.update');
+
+// Delete
+Route::delete('/delete/{id}', [PpdbsdController::class, 'destroy'])->name('ppdbsd.destroy');
+
+// Detail
+Route::get('/detail/{id}', [PpdbsdController::class, 'show'])->name('ppdbsd.show');
